5a8c455ee4a7242aa3243bbd750e17f2d3691ebc
kenji-iwatani <kleversonkenji@gmail.com>
2023-10-21 21:29:24 -0300

    feat: adds more info to the table

--- src/components/ArtistsTable.tsx
+++ src/components/ArtistsTable.tsx
@@ -1,4 +1,5 @@
 import { useMemo } from "react";
+import { ArtistsTablePagination } from "./ArtistsTablePagination";
 import useArtistList from "../hooks/useArtistList";
 
 const tableHeadCellStyle = {
@@ -14,13 +15,13 @@ const tableBodyCellStyle = {
 };
 
 export function ArtistsTable() {
-  const { artists, artistsTotal, isLoading } = useArtistList();
+  const { artists, isLoading } = useArtistList();
 
   const rows = useMemo(() => {
     if (artists.length === 0) {
       return (
         <tr>
-          <td style={tableBodyCellStyle} colSpan={2}>
+          <td style={tableBodyCellStyle} colSpan={3}>
             Lista vazia
           </td>
         </tr>
@@ -33,6 +34,9 @@ export function ArtistsTable() {
           <tr key={artist.id}>
             <td style={tableBodyCellStyle}>{artist.id}</td>
             <td style={tableBodyCellStyle}>{artist.name}</td>
+            <td style={{ ...tableBodyCellStyle, padding: "0.25rem" }}>
+              <img src={artist.imageUrl} alt={artist.name} />
+            </td>
           </tr>
         ))}
       </>
@@ -44,21 +48,25 @@ export function ArtistsTable() {
       {isLoading ? (
         <p>Carregando...</p>
       ) : (
-        <table
-          style={{
-            borderCollapse: "collapse",
-            width: "100%",
-            border: "1px solid #ddd",
-          }}
-        >
-          <thead>
-            <tr>
-              <th style={tableHeadCellStyle}>Id</th>
-              <th style={tableHeadCellStyle}>Artista</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </table>
+        <div>
+          <table
+            style={{
+              borderCollapse: "collapse",
+              width: "100%",
+              border: "1px solid #ddd",
+            }}
+          >
+            <thead>
+              <tr>
+                <th style={tableHeadCellStyle}>Id</th>
+                <th style={tableHeadCellStyle}>Artista</th>
+                <th style={tableHeadCellStyle}>Img</th>
+              </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+          </table>
+          <ArtistsTablePagination />
+        </div>
       )}
     </div>
   );
