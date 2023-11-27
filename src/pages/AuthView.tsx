d8f9610a561073543b30aa319b549a556c29325e
iwataniKenji <kleversonkenji@gmail.com>
2023-11-27 19:23:18 -0300

    chore: adiciona campos para autenticação

--- src/pages/AuthView.tsx
+++ src/pages/AuthView.tsx
@@ -1,4 +1,14 @@
+import { useState } from "react";
+
 export function AuthView() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("email:", email);
+    console.log("senha:", password);
+  };
+
   return (
     <div
       style={{
@@ -10,7 +20,37 @@ export function AuthView() {
         alignItems: "center",
       }}
     >
-      <p>teste</p>
+      <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem" }}>
+        <label>Email:</label>
+
+        <input
+          placeholder="Digite seu email"
+          style={{ padding: "0.5rem", width: 300 }}
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label>Senha:</label>
+        <input
+          placeholder="Digite sua senha"
+          style={{ padding: "0.5rem", width: 300 }}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          style={{
+            padding: "0.5rem 3rem",
+            marginTop: "0.5rem",
+            cursor: "pointer",
+          }}
+          onClick={handleLogin}
+        >
+          Login
+        </button>
+      </div>
     </div>
   );
 }
