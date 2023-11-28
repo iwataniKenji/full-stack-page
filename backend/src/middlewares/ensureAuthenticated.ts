d5ec066a66603a16d118e9fd85d779fc3926b4f1
iwataniKenji <kleversonkenji@gmail.com>
2023-11-27 22:08:30 -0300

    feat: cria middleware de verificação de auth

---
+++ backend/src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const secretKey = process.env.JWT_SECRET || 'secret';
+
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+  const token = req.headers['authorization'] as string;
+
+  if (!token) {
+    return res.status(403).json({ message: 'Token não encontrado' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Token inválido' });
+    }
+
+    (req as any).user = decoded;
+    next();
+  });
+}
